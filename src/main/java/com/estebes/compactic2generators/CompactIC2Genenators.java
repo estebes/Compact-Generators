dd4d148f05b2d25332532aa2c5e8f7216e76002a
estebes <vitorandrade.xtbx@gmail.com>
2015-06-13 01:23:33 +0100

    Stuff

---
+++ src/main/java/com/estebes/compactic2generators/CompactIC2Genenators.java
@@ -0,0 +1,32 @@
+package com.estebes.compactic2generators;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = "CompactIC2Generators", name = "Compact IC2 Generators", version = "1.7.10-0.0.1")
+public class CompactIC2Genenators
+{
+    @Mod.Instance
+    public static CompactIC2Genenators instance;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent preinit)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent init)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent postinit)
+    {
+
+    }
+
+}
